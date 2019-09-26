5046b5f0ca14ce8e4430d3738f28214a7cc059b1
gilvaju <contato@gilvancosta.com>
2019-09-26 12:05:01 -0300

    add all files for Sales domains rules

---
+++ database/migrations/2019_09_26_145208_create_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('date_sell');
+            $table->integer('pieces_trade');
+            $table->float('value_trade');
+            $table->integer('pieces_sell');
+            $table->float('value_sell');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::resource('sales', 'SalesController');
+
 Route::get('/home', 'HomeController@index')->name('home');
