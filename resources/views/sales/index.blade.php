42fb16e92afdb0554bf52013c2157f64635887eb
gilvaju <contato@gilvancosta.com>
2019-09-30 13:37:59 -0300

    general adjustments

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -22,10 +22,10 @@
                             <tr>
                                 <th scope="row">{{ $sale->id }}</th>
                                 <td>{{ $sale->date }}</td>
-                                <td>{{ $sale->trade_pieces }}</td>
-                                <td>{{ $sale->trade_value }}</td>
-                                <td>{{ $sale->pieces }}</td>
-                                <td>{{ $sale->value }}</td>
+                                <td>{{ $sale->trade_pieces }} peças</td>
+                                <td>R$ {{ $sale->trade_value }}</td>
+                                <td>{{ $sale->pieces }} peças</td>
+                                <td>R$ {{ $sale->value }}</td>
                                 <td>
                                     <form action="{{ route('sales.destroy', $sale->id) }}" method="POST">
                                         @csrf   
@@ -45,31 +45,31 @@
                         @csrf   
                         <div class="input-group mb-3 flex-nowrap">
                             <div class="input-group-prepend">
-                                <span class="input-group-text" id="date">1</span>
+                                <span class="input-group-text" id="date">/</span>
                             </div>
                             <input type="date" class="form-control" name="date" placeholder="Data" aria-label="Data" aria-describedby="date">
                         </div>
                         <div class="input-group mb-3 flex-nowrap">
                             <div class="input-group-prepend">
-                                <span class="input-group-text" id="trade_pieces">2</span>
+                                <span class="input-group-text" id="trade_pieces">QTD</span>
                             </div>
                             <input type="number" class="form-control" name="trade_pieces" placeholder="Peças de Troca" aria-label="Peças de Troca" aria-describedby="trade_pieces">
                         </div>
                         <div class="input-group mb-3 flex-nowrap">
                             <div class="input-group-prepend">
-                                <span class="input-group-text" id="trade_value">3</span>
+                                <span class="input-group-text" id="trade_value">R$</span>
                             </div>
                             <input type="number" step="0.01" class="form-control" name="trade_value" placeholder="Valor de Troca" aria-label="Valor de Troca" aria-describedby="trade_value">
                         </div>
                         <div class="input-group mb-3 flex-nowrap">
                             <div class="input-group-prepend">
-                                <span class="input-group-text" id="pieces">4</span>
+                                <span class="input-group-text" id="pieces">QTD</span>
                             </div>
                             <input type="number" class="form-control" name="pieces" placeholder="Peças da Venda" aria-label="Peças da Venda" aria-describedby="pieces">
                         </div>
                         <div class="input-group mb-3 flex-nowrap">
                             <div class="input-group-prepend">
-                                <span class="input-group-text" id="value">5</span>
+                                <span class="input-group-text" id="value">R$</span>
                             </div>
                             <input type="number" step="0.01" class="form-control" name="value" placeholder="Valor da Venda" aria-label="Valor da Venda" aria-describedby="value">
                         </div>
